327c11f8794c4decd3c3ac149669ebc312ae769c
BenHigh <ben.high@austin.rr.com>
2017-11-06 16:38:07 -0600

    finish game logic

--- hangman.js
+++ hangman.js
@@ -4,8 +4,6 @@ var Letters = require("./letters.js");
 
 var inq = require("inquirer");
 
-var wrong = 0;
-
 var guessArr = [];
 
 var allGallows = [
@@ -13,11 +11,13 @@ var allGallows = [
 ["||   |"],
 [["||"], ["||  (_)"]],
 [["||"], ["||   |"]],
-[["||"], ["|| /||"], ["|| /||\\"]],
-[["||"], ["||  /"], ["||  /\\"]],
+[["||"], ["||  /|"], ["||  /|\\"]],
+[["||"], ["||   /"], ["||   /|"]],
 ["========"]];
 
 var curGallow = [];
+
+var wrong = 0;
 
 var oWord;
 
@@ -32,7 +32,6 @@ var chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
 
 // 	for(i in allGallows){
 // 		if(i >= 2 && i <= 5){
-// 			console.log(wrong % n);
 // 			curGallow.push(allGallows[i][wrong % n]);
 // 			n += allGallows[i].length - 1;
 // 		}
@@ -43,57 +42,54 @@ var chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
 // 	}
 // }
 
-// function printGallows(){
-// 	if(done == true && wrong < 6){
-// 		console.log("YOU WIN");
-// 		console.log("Press any key to play again");
-// 	}else{
-// 		if(wrong < 6 && wrong > 0){
-// 		  console.log(gArray[wrong][1]);
-// 		}else if(wrong!=0){
-// 		  console.log("YOU LOSE");
-// 		}
-// 	}
-// }
-
-// function showWord() {
-// 	var divword = "";
-// 	var tempword = oWord.word.split('');
-// 	console.log("wrong " + wrong);
-
-// 	if(wrong == 6){
-// 		done = true;
-// 	}
-
-// 	for(i in tempword){
-// 		if(!guessArr.includes(tempword[i]) && !done){
-// 			divword+=" -";
-// 		}
-// 		else{
-// 			divword+=(" "+tempword[i]);
-// 		}
-// 	}
-// 	done = !divword.includes("-");
-// 	console.log(divword);
-// }
-
-// function updateScore() {
-// 	pWord = new Letter(oWord.word);
-// 	pWord.setWord(guessArr); 
-
-// 	if(wrong == 6){
-// 		done = true;
-// 	}
-
-// 	done = !pWord.divword.includes("-");
-
-// 	console.log(pWord.divword);
-
-// 	console.log((6-wrong) + " guesses remaining...");
-// 	var spacedguess = guessArr.sort().join(", ");
-// 	console.log(spacedguess);
-// 	printGal();
-// }
+function printGallows(){
+	if(done == true && wrong < 6){
+		console.log("YOU WIN");
+		console.log("Press any key to play again");
+	}else{
+		if(wrong === 6){
+			console.log("You lose...");
+		}
+		for(var i = 0; i < allGallows.length; i++){
+			if(i === 2){
+				if(wrong >= 1){
+					console.log(String(allGallows[i][1]));
+				}else{
+					console.log(String(allGallows[i][0]));
+				}
+			}else if(i === 3){
+				if(wrong >= 2){
+					console.log(String(allGallows[i][1]));
+				}else{
+					console.log(String(allGallows[i][0]));
+				}
+			}else if(i === 4){
+				if(wrong === 3){
+					console.log(String(allGallows[i][1]));
+				}else if(wrong >= 4){
+					console.log(String(allGallows[i][2]));
+				}else{
+					console.log(String(allGallows[i][0]));
+				}
+			}else if(i === 5){
+				if(wrong === 5){
+					console.log(String(allGallows[i][1]));
+				}else if(wrong >= 5){
+					console.log(String(allGallows[i][2]));
+				}else{
+					console.log(String(allGallows[i][0]));
+				}
+			}else{
+				console.log(String(allGallows[i][0]));
+			}
+		}
+		if(wrong < 6 && wrong > 0){
+		  console.log(allGallows[wrong][1]);
+		}else if(wrong!=0){
+		  console.log("YOU LOSE");
+		}
+	}
+}
 
 function newRound(){
 	oWord = new Word();
@@ -106,6 +102,13 @@ function runGame(){
 	var lets = new Letters(oWord.word);
 	lets.setWord(guessArr);
 
+	done = !lets.divword.includes("-");
+	if(wrong == 6){
+		done = true;
+	}
+
+	printGallows();
+
 	console.log(lets.divword);
 
 	inq.prompt([
@@ -115,11 +118,21 @@ function runGame(){
 			name: "guess"
 		}
 	]).then(function(resp){
-		if(chars.includes(resp.guess.toLowerCase())){
+		if(chars.includes(resp.guess.toLowerCase()) && !guessArr.includes(resp.guess)){
+			if(lets.tempword.includes(resp.guess.toLowerCase())){
+				console.log("You are correct!");
+			}else{
+				console.log("You are incorrect...");
+				wrong++;
+			}
 			guessArr.push(resp.guess.toLowerCase());
+		}else if(guessArr.includes(resp.guess)){
+			console.log("Enter a character you haven't tried before");
 		}else{
-			console.log("Enter a valid character")
+			console.log("Enter a valid character");
 		}
+
+
 		runGame();
 	});
 };
